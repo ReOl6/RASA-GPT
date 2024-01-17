a85b9d3674aae13a078760b78ff13842166af88b
Pure122 <89637166+purekkl@users.noreply.github.com>
2024-01-17 22:15:22 +0700

    fallback

--- actions/actions.py
+++ actions/actions.py
@@ -2,7 +2,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, EventType
 from rasa_sdk.types import DomainDict
 import pandas as pd
 
@@ -12,6 +12,13 @@ import openai
 course = pd.read_csv('Course.csv')
 years = ','.join(pd.unique(course.copy()['Year']).astype(str))
 itf = pd.read_csv('06026100.csv')
+
+def searhingcouseofyear(year, course):
+    coursey = course[course['Year'] == year]
+    df = coursey[['course_id','course_name']]
+    return df.to_string()
+
+    
 
 class ValidateCourseForm(FormValidationAction):
     def name(self) -> Text:
@@ -28,8 +35,8 @@ class ValidateCourseForm(FormValidationAction):
         if str(slot_value.lower()) not in years:
             dispatcher.utter_message(text=f'We have only {years} year.')
             return {'year': None}
-        dispatcher.utter_message(text=f'OK! This is courses of {slot_value} year.')
-        return {'year': slot_value}
+        dispatcher.utter_message(text=f'OK! This is courses of {slot_value} year.\n{searhingcouseofyear(slot_value, course)}')
+        return
 
 class ActionFallback(Action):
 
@@ -40,7 +47,7 @@ class ActionFallback(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        openai.api_key = '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'
+        openai.api_key = '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'
 
 
         prompt = """ 
@@ -61,7 +68,7 @@ class ActionFallback(Action):
         question = "\n\n user: "+tracker.latest_message["text"][0:]
         hist_slot = " "
         hist_slot = str(tracker.slots.get('hist_slot'))+question
-        #hist_slot stores the history fo the conversation 
+        #hist_slot stores the history fo the conversation
 
         #pmt is the ffinal prompt to send to open ai
         pmt  = prompt + hist_slot + " \n ---\n reply to only this text :"+question
@@ -80,3 +87,19 @@ class ActionFallback(Action):
         hist_slot = hist_slot + "\nBot: "+response
 
         return [SlotSet("hist_slot", hist_slot)]
+
+# class ActionNLUFallback(Action):
+#     def name(self) -> Text:
+#         return "action_default_fallback"
+
+#     def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[EventType]:
+#         action_events = [e for e in tracker.events if e["event"] == "action"]
+#         if len(action_events) > 1 and action_events[-2].get("name") == "action_default_fallback":
+#               dispatcher.utter_message(template="utter_goodbye")
+#         else:
+#               dispatcher.utter_message(template="utter_default")
